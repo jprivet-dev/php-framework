06ac315da03341c96cb250786b8228e8ce5fb911
jprivet-dev <jean.philippe.rivet@gmail.com>
2023-03-06 17:47:15 +0100

    feat(index): use symfony httpfoundation

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
+require_once __DIR__.'/vendor/autoload.php';
 
-$name = $_GET['name'] ?? 'World';
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-header('Content-Type: text/html; charset=utf-8');
+$request = Request::createFromGlobals();
 
-printf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8'));
+$name = $request->query->get('name', 'World');
+
+$response = new Response(sprintf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
+
+$response->send();
