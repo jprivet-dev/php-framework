e2f48f6ea67ff68035a19f4eec5c4ba0119e03d7
jprivet-dev <jean.philippe.rivet@gmail.com>
2023-03-06 21:49:12 +0100

    refactor(app): create pages & web folders

--- web/front.php
+++ web/front.php
@@ -3,20 +3,20 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
 
 $request = Request::createFromGlobals();
 $response = new Response();
 
 $map = [
-    '/hello' => __DIR__.'/hello.php',
-    '/bye' => __DIR__.'/bye.php',
+    '/hello' => 'hello.php',
+    '/bye' => 'bye.php',
 ];
 
 $path = $request->getPathInfo();
 if (isset($map[$path])) {
-    require $map[$path];
-} else{
+    require __DIR__.'/../src/pages/'.$map[$path];
+} else {
     $response->setStatusCode(404);
     $response->setContent('Not Found');
 }
