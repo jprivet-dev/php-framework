6b68070ba554f1a6aba3ecc3b7f81a870b42a2c3
jprivet-dev <jean.philippe.rivet@gmail.com>
2023-03-06 22:29:16 +0100

    feat(pages): convert all pages into templates

--- src/pages/hello.php
+++ src/pages/hello.php
@@ -1,3 +1,2 @@
-<?php
-$name = $request->query->get('name', 'World');
-$response->setContent(sprintf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
+<?php $name = $request->query->get('name', 'World') ?>
+Hello <?= htmlspecialchars($name, ENT_QUOTES, 'UTF-8') ?>
