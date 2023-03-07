69156f4165520018c13ad703ec4a2ac2c6a60ee2
jprivet-dev <jean.philippe.rivet@gmail.com>
2023-03-07 21:45:13 +0100

    feat(LeapYearControllerTest): split tests

--- tests/Simplex/FrameworkTest.php
+++ tests/Simplex/FrameworkTest.php
@@ -1,12 +1,9 @@
 <?php
 
-use Controller\LeapYearController;
 use PHPUnit\Framework\TestCase;
 use Simplex\Framework;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
 use Symfony\Component\HttpKernel\Controller\ArgumentResolverInterface;
-use Symfony\Component\HttpKernel\Controller\ControllerResolver;
 use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
@@ -46,35 +43,5 @@ class FrameworkTest extends TestCase
         $argumentResolver = $this->createMock(ArgumentResolverInterface::class);
 
         return new Framework($matcher, $controllerResolver, $argumentResolver);
-    }
-
-    public function testLeapYearControllerResponse()
-    {
-        $context = $this->createMock(RequestContext::class);
-
-        $matcher = $this->createMock(UrlMatcherInterface::class);
-        $matcher
-            ->expects($this->once())
-            ->method('match')
-            ->will(
-                $this->returnValue([
-                    '_route' => 'is_leap_year/{year}',
-                    'year' => '2000',
-                    '_controller' => [new LeapYearController(), 'index'],
-                ])
-            );
-        $matcher
-            ->expects($this->once())
-            ->method('getContext')
-            ->will($this->returnValue($context));
-
-        $controllerResolver = new ControllerResolver();
-        $argumentResolver = new ArgumentResolver();
-
-        $framework = new Framework($matcher, $controllerResolver, $argumentResolver);
-        $response = $framework->handle(new Request());
-
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertEquals('Yep, 2000 is a leap year.', $response->getContent());
     }
 }
