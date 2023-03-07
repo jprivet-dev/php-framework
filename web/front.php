f399cfc61da6eb3bac084a3222e248031cb06197
jprivet-dev <jean.philippe.rivet@gmail.com>
2023-03-07 10:38:40 +0100

    feat(router): use symfony router

--- web/front.php
+++ web/front.php
@@ -2,25 +2,41 @@
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
 
 require_once __DIR__.'/../vendor/autoload.php';
 
 $request = Request::createFromGlobals();
 $response = new Response();
 
-$map = [
-    '/hello' => 'hello',
-    '/bye' => 'bye',
-];
+$routes = new RouteCollection();
+$routes->add(
+    'hello',
+    new Route('/hello/{name}', [
+        'name' => 'World',
+    ])
+);
+$routes->add('bye', new Route('/bye'));
 
-$path = $request->getPathInfo();
-if (isset($map[$path])) {
+$context = new RequestContext();
+$context->fromRequest($request);
+$matcher = new UrlMatcher($routes, $context);
+
+try {
+    // Request attributes are extracted to keep our templates simple
+    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
     ob_start();
-    extract($request->query->all(), EXTR_SKIP);
-    require sprintf(__DIR__.'/../src/pages/%s.php', $map[$path]);
+    require sprintf(__DIR__.'/../src/pages/%s.php', $_route); // Route names are used for template names
     $response = new Response(ob_get_clean());
-} else {
+} catch (ResourceNotFoundException $exception) {
     $response = new Response('Not Found', 404);
+} catch (Exception $exception) {
+    // 500 errors are now managed correctly
+    $response = new Response('An error occurred', 500);
 }
 
 $response->send();
