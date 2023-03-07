c0ddd1bbbf41f15e9bb9b8b68bccb7a14febf86f
jprivet-dev <jean.philippe.rivet@gmail.com>
2023-03-07 15:30:30 +0100

    feat(app): create & use LeapYearController

--- src/app.php
+++ src/app.php
@@ -14,6 +14,19 @@ function is_leap_year(int $year): bool
     return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
 }
 
+class LeapYearController
+{
+    public function index(Request $request): Response
+    {
+        $year = $request->attributes->get('year');
+        $content = is_leap_year($year)
+            ? sprintf('Yep, %s is a leap year.', $year)
+            : sprintf('Nope, %s is not a leap year.', $year);
+
+        return new Response($content);
+    }
+}
+
 $routes = new RouteCollection();
 
 $routes->add(
@@ -35,14 +48,7 @@ $routes->add(
     'leap_year',
     new Route('/is_leap_year/{year}', [
         'year' => date('Y'),
-        '_controller' => function (Request $request): Response {
-            $year = $request->attributes->get('year');
-            $content = is_leap_year($year)
-                ? sprintf('Yep, %s is a leap year.', $year)
-                : sprintf('Nope, %s is not a leap year.', $year);
-
-            return new Response($content);
-        },
+        '_controller' => [new LeapYearController, 'index'],
     ])
 );
 
